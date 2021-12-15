99ef27fe2c3b2be5b767aac637181df18837e073
happytoons141 <happytoons141@yahoo.com>
2021-12-15 10:01:04 +0000

    Fix set2 tests for pytest

--- GistTypes/src/Patch/Set2/test_me.py
+++ GistTypes/src/Patch/Set2/test_me.py
@@ -1,13 +1,15 @@
 from unittest.mock import patch
-import main
+import GistTypes.src.Patch.Set2.main as main
 
 def test_f_a():
     MOCK_RETVAL = 48
-    with patch("main.f_b") as f_b_mock:
+    with patch('GistTypes.src.Patch.Set2.main.f_b') as f_b_mock:
         # arrange
         f_b_mock.return_value = MOCK_RETVAL
         # act
         ret = main.f_a()
+        print(f"\nret: {ret}")
+        print(f"MOCK_RETVAL: {MOCK_RETVAL}")
         # assert
         assert main.f_a() == MOCK_RETVAL + 1
 
