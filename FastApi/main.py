b3f6b99472674c421fcac9f99ac6fe1430e97548
happytoons141 <happytoons141@yahoo.com>
2021-12-23 18:38:17 +0000

    looping over json in html template

--- FastApi/main.py
+++ FastApi/main.py
@@ -9,10 +9,14 @@
 # https://python-poetry.org/docs/#installation
 """
 
-from fastapi import FastAPI, Request
+
+from fastapi import FastAPI, Request, APIRouter
 from fastapi.templating import Jinja2Templates
 
+
 app = FastAPI()
+api_router = APIRouter()
+
 templates = Jinja2Templates(directory="templates/")
 data = [
     {"id":1,"first_name":"Layla","last_name":"Cohn","email":"lcohn0@g.co",
@@ -26,12 +30,12 @@ data = [
     {"id":5,"first_name":"Ellsworth","last_name":"McCarlich","email":"emccarlich4@mit.edu",
         "ip_address":"43.123.149.96","trade":"Plasterers"}]
 
-@app.get("/")
-async def root():
-    """
-    Render the chunk of fake json data
-    """
-    return data
+# @app.get("/")
+# async def root():
+#     """
+#     Render the chunk of fake json data
+#     """
+#     return data
 
 @app.get("/form")
 def form_post(request: Request):
@@ -41,3 +45,11 @@ def form_post(request: Request):
     """
     # http://127.0.0.1:8000/form
     return templates.TemplateResponse('index.html', context={'request': request, 'result': data})
+
+
+@app.get('/')
+def root(request: Request) -> dict:
+    return templates.TemplateResponse(
+        'index.html',
+        {'request': request, 'result': data} 
+    )
