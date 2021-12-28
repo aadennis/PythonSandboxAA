f3a5ac935cbe03d7946d555a4e3c5554d1941062
happytoons141 <happytoons141@yahoo.com>
2021-12-28 22:24:25 +0000

    From uy course

---
+++ IdesEtc/StreamLit/uy/demouy01.py
@@ -0,0 +1,20 @@
+#streamlit run IdesEtc/StreamLit/uy/demouy01.py
+import streamlit as st
+import pandas
+
+data = {
+    "Series_1": [1,3,4,5,7],
+    "Series_2": [10,30,40,100,250]
+}
+
+df = pandas.DataFrame(data)
+
+
+st.title("This be the title")
+st.subheader("This be the sub-header")
+st.write('''This be body text. 
+These extra words appear on the same line, not a
+different line.
+''')
+
+st.write(df)
