792e453345a5b9063c87acf0cd8a4100d92bf01e
happytoons141 <happytoons141@yahoo.com>
2021-12-29 16:59:13 +0000

    save video to symlink

--- VideoHandling/src/yt-dlp.py
+++ VideoHandling/src/yt-dlp.py
@@ -1,5 +1,12 @@
 # https://github.com/yt-dlp/yt-dlp
 # python3 ./yt-dlp.py
+# If target_leaf (see code) does not exist, it will be created
+# in wsl/linux, to create a symlink to the usual place...
+# 1. ln_target='/mnt/e/Zoolz/C/Videos/YouTube/Others'
+# 2. sudo ln -s $ln_target /wintemp
+# So... ls -l /wintemp 
+# lrwxrwxrwx 1 root root 36 Dec 29 16:36 /wintemp -> /mnt/e/Zoolz/C/Videos/YouTube/Others
+
 
 import datetime
 from sys import platform
@@ -11,17 +18,18 @@ def download_yt_video():
     yt_prefix = "https://www.youtube.com/watch?v="
 
     if (platform == 'linux'):
-        SAVE_PATH = "/tmp" 
-        #\\wsl$\Ubuntu-20.04\tmp
+        SAVE_PATH = "/wintemp" 
     else: # assume win32
         raise ValueError
 
     if not os.path.exists(ytexe):
-        # non-checked in static copy: D:\software\VideoSoftware\YoutubeDownloader\yt-dlp 
+        # non-checked-in static copy: D:\software\VideoSoftware\YoutubeDownloader\yt-dlp 
         raise FileNotFoundError("no file found: [{}]. Exiting...".format(ytexe))
 
-    link = input("Paste the YouTube URL (only the part aafter 'v='): ")        
-    command_line = f"{ytexe} {yt_prefix}{link} -o '{SAVE_PATH}/%(title)s-%(id)s'.mp4"
+    link = input("Paste the YouTube URL (only the part aafter 'v='): ")  
+    target_leaf = input(f"What is the folder under {SAVE_PATH} to save the video? (return = none): ")      
+    output_template = f"{SAVE_PATH}/{target_leaf}/%(title)s-%(id)s"
+    command_line = f"{ytexe} {yt_prefix}{link} -o '{output_template}'.mp4"
     print(f"[cmd line]: {command_line}")
 
     os.system(command_line)
