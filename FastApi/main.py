39e125e25b07f74744e1c942f9c40db36d553e5d
aadennis <dennis.circ@gmail.com>
2022-03-02 15:35:28 +0000

    Refactor data to model.py

--- FastApi/main.py
+++ FastApi/main.py
@@ -1,7 +1,7 @@
 """
 # https://fastapi.tiangolo.com/tutorial/first-steps/#first-steps
 # https://mockaroo.com/
-# PYTHONPATH=~/git/PythonSandbox
+# PYTHONPATH=~/git/aadennis/PythonSandboxAA
 # cd $PYTHONPATH/FastApi
 # uvicorn main:app --reload
 # http://127.0.0.1:8000
@@ -12,23 +12,13 @@
 
 from fastapi import FastAPI, Request, APIRouter
 from fastapi.templating import Jinja2Templates
+from model import data
 
 
 app = FastAPI()
 api_router = APIRouter()
 
 templates = Jinja2Templates(directory="templates/")
-data = [
-    {"id":1,"first_name":"Layla","last_name":"Cohn","email":"lcohn0@g.co",
-        "ip_address":"229.77.172.67","trade":"Equipment Operator"},
-    {"id":2,"first_name":"Padget","last_name":"Bourbon","email":"pbourbon1@usatoday.com",
-        "ip_address":"216.136.157.57","trade":"Millwright"},
-    {"id":3,"first_name":"Jake","last_name":"Ecclestone","email":"jecclestone2@liveinternet.ru",
-        "ip_address":"155.255.88.47","trade":"Plumber"},
-    {"id":4,"first_name":"Adair","last_name":"Worgen","email":"aworgen3@ucoz.ru",
-        "ip_address":"82.192.233.142","trade":"Carpenter"},
-    {"id":5,"first_name":"Ellsworth","last_name":"McCarlich","email":"emccarlich4@mit.edu",
-        "ip_address":"43.123.149.96","trade":"Plasterers"}]
 
 # @app.get("/")
 # async def root():
