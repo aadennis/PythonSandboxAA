ef863f9c5edb82aa1e7b6db596af04cce98eef76
aadennis <dennis.circ@gmail.com>
2022-03-15 09:15:34 +0000

    move to new folder Downloader

---
+++ VideoHandling/Downloader/src/archive/cli/README.md
@@ -0,0 +1,26 @@
+# yt-dlp.py - wrapper for yt-dlp (YT downloader)
+
+## Usage (Windows):
+![image](https://user-images.githubusercontent.com/61011995/154809643-6553100b-2239-4bed-8666-c97ff0f51b7e.png)
+
+<img width="1103" alt="image" src="https://user-images.githubusercontent.com/61011995/154809980-f0a38969-de6c-4c2f-96f9-d6aba22170a4.png">
+
+![image](https://user-images.githubusercontent.com/61011995/154809702-311d9348-18cf-4619-8eb7-28fde0e29ea1.png)
+
+![image](https://user-images.githubusercontent.com/61011995/154809724-c3470292-3c50-4451-b268-35c20becb189.png)
+
+<img width="832" alt="image" src="https://user-images.githubusercontent.com/61011995/154809786-d11db2b8-b932-4eb4-8ee4-5a17ef0a8939.png">
+
+![image](https://user-images.githubusercontent.com/61011995/154809811-9abeb308-48d3-4fc6-bc19-8ed90f800887.png)
+
+<img width="568" alt="image" src="https://user-images.githubusercontent.com/61011995/154809886-8d730493-87ac-4650-a000-8d4160ba2918.png">
+
+
+![image](https://user-images.githubusercontent.com/61011995/154809922-e0bc4a36-6d60-40d0-9888-e0e11183dec2.png)
+
+
+Video is single video - default
+List is a list of videos, as indicated by the argument after the "list=" parameter below:
+
+![image](https://user-images.githubusercontent.com/61011995/156021871-99293b7d-287a-40a2-96f7-416e92fdcd6f.png)
+
---
+++ VideoHandling/Downloader/src/ytdl_view.py
@@ -0,0 +1,47 @@
+# Windows (Powershell)
+# cd d:\Sandbox\git\aadennis\
+# .\virtualenvs\venv\Scripts\activate (Windows)
+# source .\virtualenvs\venv\Scripts\activate (Git Bash on Windows)
+# cd .\PythonSandboxAA\VideoHandling\Downloader
+# uvicorn src.ytdl_view:app --reload
+# http://127.0.0.1:8000/video
+# Testing:
+# My own Youtube video (6mb) is here: FFs4JIUbXJU
+# https://github.com/tiangolo/fastapi/issues/854
+
+from typing import Optional
+from urllib import response
+from fastapi import FastAPI, Request, Form
+from fastapi.templating import Jinja2Templates
+from starlette.responses import FileResponse, Response
+from fastapi.staticfiles import StaticFiles
+
+from src.ytdl_model import save_video
+
+app = FastAPI()
+templates = Jinja2Templates(directory='src/templates/')
+
+app.mount("/static", app = StaticFiles(directory="static"), name="static")
+
+@app.get('/')
+def read_form():
+    return 'video download'
+
+@app.get('/video')
+def form_post(request: Request):
+    results = ['Awaiting [Save] action...']
+    return templates.TemplateResponse('ytdl.html', context={'request': request, 'result': results})
+
+async def app2(scope, receive, send):
+    assert scope['type'] == 'http'
+    response = FileResponse('static/favicon.ico')
+    await response(scope, receive, send)
+
+@app.post('/video')
+async def form_post(request: Request, action: str = Form(...), 
+    video_code: Optional[str] = Form(None), sub_folder: Optional[str] = Form(None), single_or_list: str = Form(...)):
+    if action == 'Save':
+        results = save_video(video_code, single_or_list, sub_folder)
+        return templates.TemplateResponse('ytdl.html', context={'request': request, 'result': results})
+        
+
