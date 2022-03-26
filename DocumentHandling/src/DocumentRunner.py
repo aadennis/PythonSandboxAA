272ddfa3efa9199b659e9cdfd13ed15a01d72682
aadennis <dennis.circ@gmail.com>
2022-03-26 08:37:00 +0000

    Add defaults to command line

--- DocumentHandling/src/DocumentRunner.py
+++ DocumentHandling/src/DocumentRunner.py
@@ -1,3 +1,4 @@
+import os
 from Document import Document
 """
     Instantiate and run Document for its
@@ -5,13 +6,29 @@ from Document import Document
 """
 
 def NumberHeadings():
-    sourceDoc = input("Enter the full path of the source text file: ")
-    targetDoc = input("Enter the full path of the target text file: ")
+    default_folder = "c:/temp"
+    default_source_file = "source.txt"
+    default_target_file = "target.txt"
     
-    documentLineSet = Document.file_to_DocumentLineDict(sourceDoc)
+    in_folder = input(f"Enter the folder for both source and target files (default [{default_folder}]): ")
+    in_sourceDoc = input(f"Enter the name of the source text file (default [{default_source_file}]): ")
+    in_targetDoc = input(f"Enter the full path of the target text file( default [{default_target_file}]): ")
+
+    folder = default_folder if in_folder == '' else in_folder
+    sourceDoc = default_source_file if in_sourceDoc == '' else in_sourceDoc
+    targetDoc = default_target_file if in_targetDoc == '' else in_targetDoc
+    
+    source_path = os.path.join(folder, sourceDoc)
+    target_path = os.path.join(folder, targetDoc)
+    print(folder)
+    print(sourceDoc)
+    print(targetDoc)
+    
+    documentLineSet = Document.file_to_DocumentLineDict(source_path)
     doc = Document(documentLineSet)
     docWithHeadings = doc.number_all_headers()
-    doc.dict_values_to_file(docWithHeadings, targetDoc)
+    doc.dict_values_to_file(docWithHeadings, target_path)
+    print(f"File with numbered headings is at [{target_path}]")
 
 
 if __name__ == '__main__':
