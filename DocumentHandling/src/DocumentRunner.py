4d4b14be9bc13144a1d98b128a9c588af65b8496
aadennis <dennis.circ@gmail.com>
2022-03-28 15:16:26 +0100

    get cmdline working again

--- DocumentHandling/src/DocumentRunner.py
+++ DocumentHandling/src/DocumentRunner.py
@@ -8,20 +8,15 @@ from Document import Document
 def NumberHeadings():
     default_folder = "c:/temp"
     default_source_file = "source.txt"
-    default_target_file = "target.txt"
     
     in_folder = input(f"Enter the folder for both source and target files (default [{default_folder}]): ")
     in_sourceDoc = input(f"Enter the name of the source text file (default [{default_source_file}]): ")
-    in_targetDoc = input(f"Enter the full path of the target text file( default [{default_target_file}]): ")
 
     folder = default_folder if in_folder == '' else in_folder
     sourceDoc = default_source_file if in_sourceDoc == '' else in_sourceDoc
-    targetDoc = default_target_file if in_targetDoc == '' else in_targetDoc
     
     source_path = os.path.join(folder, sourceDoc)
-    target_path = os.path.join(folder, targetDoc)
     print(f"Source file location: [{source_path}]")
-    print(f"Target file location: [{target_path}]")
 
     if not os.path.exists(source_path):
         print("*****************************************************************")
@@ -29,12 +24,9 @@ def NumberHeadings():
         print("*****************************************************************")
         exit(1)
     
-    documentLineSet = Document.file_to_DocumentLineDict(source_path)
-    doc = Document(documentLineSet)
-    docWithHeadings = doc.number_all_headers()
-    doc.dict_values_to_file(docWithHeadings, target_path)
-    print(f"File with numbered headings is at [{target_path}]")
-
+    lines = Document.file_to_DocumentLineDict(source_path)
+    doc = Document(lines)
+    doc.save_number_headings_to_file(folder)
 
 if __name__ == '__main__':
     NumberHeadings()
