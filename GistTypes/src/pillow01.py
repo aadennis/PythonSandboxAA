e99eb52644d6acac47ae0e76bf5d842a96f210f8
aadennis <dennis.circ@gmail.com>
2022-03-31 20:12:49 +0100

    again

--- GistTypes/src/pillow01.py
+++ GistTypes/src/pillow01.py
@@ -3,26 +3,41 @@ import io
 import glob
 import os
 
-dir = "D:\onedrive\data\photos\_Albums\CameraRollDump\CRD_2021_10"
-wildcard = "*ioS.png"
-print(dir)
-joint = dir + "/" + wildcard
+"""
+Convert a batch of png files to jpg, so that the tags field is accessible in the exif metadata.
+The source file is not changed or deleted.
+RN, the function only supports source files with the format "*iOS.png" as my interest is in 
+IOS photos
+"""
+def convert_fileset_from_png_to_jpg(folder):
 
-f = glob.glob(joint)
-# print(f)
+    PNG_WILDCARD = "*iOS.png"
+    JPG = ".JPG"
 
-for src_filename in f:
+    if not os.path.exists(folder):
+        raise FileNotFoundError(f"No folder [{folder}]. Exiting...")
+    
+    print(folder)
+    joint = folder + "/" + PNG_WILDCARD
 
-    src_base = os.path.basename(src_filename)
-    b = os.path.splitext(src_base)[0]
+    f = glob.glob(joint)
+    # print(f)
 
-    target_name = dir + "/" + b + ".jpg"
+    for src_filename in f:
 
-    print(b)
-    print(src_filename)
-    print(target_name)
+        src_base = os.path.basename(src_filename)
+        file_root = os.path.splitext(src_base)[0]
 
-    im = Image.open(src_filename)
-    rgb_im = im.convert('RGB')
-    rgb_im.save(target_name)
+        target_name = dir + "/" + file_root + JPG
 
+        print(src_filename)
+        print(target_name)
+
+        im = Image.open(src_filename)
+        rgb_im = im.convert('RGB')
+        rgb_im.save(target_name)
+
+
+if __name__ == "__main__":
+    dir = "D:\onedrive\data\photos\_Albums\CameraRollDump\CRD_2021_10"
+    convert_fileset_from_png_to_jpg(dir)
