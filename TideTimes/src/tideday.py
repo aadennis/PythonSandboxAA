65671edff541ce64fcb075fea0ddb5889ebc5ff5
aadennis <dennis.circ@gmail.com>
2022-06-26 10:15:04 +0100

    tideday dev

--- TideTimes/src/tideday.py
+++ TideTimes/src/tideday.py
@@ -5,6 +5,7 @@ class TideDay:
 
     items = None
     tide_date = None
+    tide_times = []
     
 
     def __init__(self, tide_day_record, month, first_tide_type):
@@ -14,7 +15,15 @@ class TideDay:
         # self.tide_1 = tide_1,
         # self.tide_2 = tide_2,
         # self.tide_3 = tide_3,
-        # self.tide_4 = tide_4     
+        # self.tide_4 = tide_4    
+        
+    # if an int, return True. Else false
+    def parse_int(self, candidate_int):
+        try:
+            dummy_int = int(candidate_int)
+        except ValueError:
+            return False
+        return True 
 
     def validate_record(self, tide_day_record, month):
         self.items = tide_day_record.split(",")
@@ -22,7 +31,13 @@ class TideDay:
             raise ValueError(f"Expecting stuff bud, but got {len(self.items)}")
         if 0 < month > 12:
             raise ValueError(f"{month} is not a valid month")
+        for i in range(1,5):
+            if not self.parse_int(self.items[i]):
+                raise ValueError(f"Tide data is not valid: {self.items[i]}")
+            self.tide_times.append(self.items[i])
+            
         self.tide_date = (int) (self.items[0])
+       
         
 
     def get_items(self):
--- TideTimes/test_tide/test_tideday.py
+++ TideTimes/test_tide/test_tideday.py
@@ -8,6 +8,16 @@ class TestTideDay:
         line = "29,0138013,0746407,1401000,2011390"
         td = TideDay(line,1,"low")
 
-        assert td.get_items() == 29
+        assert td.tide_date == 29
+        assert len(td.tide_times) == 4
+        assert td.tide_times[0] == "0138013"
+        assert td.tide_times[1] == "0746407"
+        assert td.tide_times[2] == "1401000"
+        assert td.tide_times[3] == "2011390"
+
+    def test_is_not_valid_tideday_record(self):
+        line = "29,a138013,0746407,1401000,2011390"
+        td = TideDay(line,1,"low")
+
 
    
