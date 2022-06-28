155d65a73a273f8fc8ff12d4c92007f2cee312f5
aadennis <dennis.circ@gmail.com>
2022-06-28 08:40:17 +0100

    July 2022 data

--- TideTimes/test_tide/test_tidemonth.py
+++ TideTimes/test_tide/test_tidemonth.py
@@ -50,6 +50,8 @@ class TestTideMonth:
         tide_month_data =[]
 
         file_path =  "TideTimes/test_tide/data/tidetimes_2022_04_v2_dictated.csv"
+        file_path =  "TideTimes/test_tide/data/tidetimes_2022_06_dictated.csv"
+        
         with io.open(file_path, 'r') as f:
             for line in f:
                 tide_month_data.append(line)
