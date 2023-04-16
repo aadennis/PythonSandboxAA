e27c1054fb6a8c9312b7ab517df37dbc334317e7
aadennis <dennis.circ@gmail.com>
2023-04-16 18:19:18 +0100

    refactoring before Whisper

--- OpenAI/OpenaiPlay.py
+++ OpenAI/OpenaiPlay.py
@@ -1,22 +1,27 @@
 # pip install openai
 # playing - chat.openai.com/chat
 # https://pypi.org/project/openai/
-
 import openai
 import util
 
-# alternative ways to get the key
-#key = getpass.getpass('Enter the api key:') # import getpass
-#key = open('key.txt').read().strip('\n')
+class OpenAiPlay(object):
+    def __init__(self):
+        self.api_key = util.get_clipboard()
+       
+    def davinci_test(self): 
+        # alternative ways to get the key
+        #key = getpass.getpass('Enter the api key:') # import getpass
+        #key = open('key.txt').read().strip('\n')
+        openai.api_key = self.api_key
+        prompt = "Write an advertising slogan for a soft drink"
+        response = openai.Completion.create(
+            model='text-davinci-003',
+            prompt=prompt,
+            temperature=0.8, # high temp = high randomness
+            max_tokens=1000 # you pay per 1k token - input and output
+        )
+        print(response)
+        # print(response['choices'][0]['message']['content'])
 
-openai.api_key = util.get_clipboard()
-prompt = "Write an advertising slogan for a soft drink"
-response = openai.Completion.create(
-    model='text-davinci-003',
-    prompt=prompt,
-    temperature=0.8, # high temp = high randomness
-    max_tokens=1000 # you pay per 1k token - input and output
-)
-print(response)
-# print(response['choices'][0]['message']['content'])
-
+oap = OpenAiPlay()
+oap.davinci_test()
