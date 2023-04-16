272c639aafb31304d62c8a26d912294e0cfd61e6
aadennis <dennis.circ@gmail.com>
2023-04-17 00:20:51 +0100

    Testing Whisper

--- OpenAI/OpenaiPlay.py
+++ OpenAI/OpenaiPlay.py
@@ -2,10 +2,13 @@
 # playing - chat.openai.com/chat
 # https://pypi.org/project/openai/
 import openai
+import os
 import util
 
 class OpenAiPlay(object):
     def __init__(self):
+        # This assumes you have pasted the api key into your clipboard before
+        # execution
         self.api_key = util.get_clipboard()
        
     def davinci_test(self): 
@@ -23,5 +26,25 @@ class OpenAiPlay(object):
         print(response)
         # print(response['choices'][0]['message']['content'])
 
+    # https://platform.openai.com/docs/guides/speech-to-text/quickstart
+    # https://platform.openai.com/docs/guides/speech-to-text/prompting
+    def whisper_test(self, speech_file):
+        openai.api_key = self.api_key
+        with open(speech_file, 'rb') as audio_file:
+            transcript = openai.Audio.transcribe(file=audio_file, model='whisper-1', prompt='High\nLow')
+            print(transcript)
+
+
+
+
 oap = OpenAiPlay()
-oap.davinci_test()
+
+#oap.davinci_test()
+
+# The passed speech file can be found in 
+# OD\data\photos\2023\2023_03\GoogleSpeech\definitive_test_input.mp3
+# Put a working copy in this OpenAI folder - mark as Ignore, as too big for GitHub
+audio_file = "OpenAI/test_artefacts/definitive_test_input.mp3"
+audio_file = "OpenAI/test_artefacts/short_tide.m4a"
+
+oap.whisper_test(audio_file)
