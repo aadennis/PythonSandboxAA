791785c84f96969289419da8c1957fb8b38d0952
aadennis <dennis.circ@gmail.com>
2023-05-16 21:56:50 +0100

    refactor to function

--- TimeUtilities/TimeDeltaUsage1.py
+++ TimeUtilities/TimeDeltaUsage1.py
@@ -1,17 +1,24 @@
-import datetime
+def apply_delta_to_times(times, minutes_interval):
+    import datetime
+    updated_times = ""
 
-TIMES = "0515 0530 0603 0633 0703 0733 1228 1243 1258 1635 1651 1710 1725 1755 2004 2020 2034 2104 2204 2304 2345 0004"
-MINUTES_INTERVAL = -6
+    times_list = times.split()
 
-# Convert the input string to a list of datetime objects.
-times_list = TIMES.split()
+    # Add the time interval to each datetime object.
+    for time in times_list:
+        new_time = datetime.datetime.strptime(
+            time, "%H%M") + datetime.timedelta(minutes=minutes_interval)
 
-# Add the time interval to each datetime object.
-for time in times_list:
-    new_time = datetime.datetime.strptime(time, "%H%M") + datetime.timedelta(minutes=MINUTES_INTERVAL)
+        # Format the new datetime object and print it out.
+        if new_time.hour == 24:
+            new_time = datetime.datetime(
+                new_time.year, new_time.month, new_time.day, 0, new_time.minute)
+        updated_times += " / " + new_time.strftime("%H:%M")
+    return updated_times
 
-    # Format the new datetime object and print it out.
-    if new_time.hour == 24:
-        new_time = datetime.datetime(new_time.year, new_time.month, new_time.day, 0, new_time.minute)
-    print(new_time.strftime("%H:%M"))
+# entry point
 
+times = "0515 0530 0603 0633 0703 0733 1228 1243 1258 1635 1651 1710 1725 1755 2004 2020 2034 2104 2204 2304 2345 0004"
+minutes_interval = -6
+updated_times = apply_delta_to_times(times, minutes_interval)
+print(updated_times)
