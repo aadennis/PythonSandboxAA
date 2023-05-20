b35b1dfaf7618969804c6145a71c5c0b87f1219b
Dennis <dennis.circ@gmail.com>
2023-05-20 19:22:31 +0100

    Report if no heic files found

--- ImageHandling/src/heic_to_jpg.py
+++ ImageHandling/src/heic_to_jpg.py
@@ -16,6 +16,7 @@ from pillow_heif import register_heif_opener
 
 path = "c:/temp/"
 files = os.listdir(path)
+heic_found = False
 register_heif_opener()
 
 for file in files:
@@ -23,7 +24,11 @@ for file in files:
     file_prefix = filename[0]
     file_extension = filename[1]
     if file_extension.upper() == ".HEIC":
+        heic_found = True
         print(f"Converting: {file}")
         im = Image.open(path+file)
         rgb_im = im.convert("RGB")
         rgb_im.save(path+file_prefix+'_from_heic'+'.jpg')
+
+if not heic_found:
+    print(f"No heic files found in folder [{path}]")
