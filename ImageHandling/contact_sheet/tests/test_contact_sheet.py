1b29d6cb957c0f2dc9f6cb6032da1ed70b920d47
aadennis <dennis.circ@gmail.com>
2023-06-28 18:32:20 +0100

    basic tests for contact sheet

--- ImageHandling/contact_sheet/tests/test_contact_sheet.py
+++ ImageHandling/contact_sheet/tests/test_contact_sheet.py
@@ -1,18 +1,15 @@
-import pytest
-import contact_sheet
+from src.contact_sheet import make_contact_sheet
 
 class TestContactSheet(object): 
-    img_folder = "test/TestImageFiles"
+    img_folder = "tests/TestImageFiles"
        
     def test_small_volume_set1(self):
         # arrange
         output_file = "c:/temp/smallstuff.jpg"
         # act
-        contact_sheet = contact_sheet.make_contact_sheet(self.img_folder, output_file)
+        contact_sheet = make_contact_sheet(self.img_folder, output_file)
 
     def test_large_volume_set1(self):
-
-
         # arrange
         # override test class default
         img_folder = "D:/onedrive/data/photos/_Albums/CameraRollDump/CRD_2023_01"
@@ -20,4 +17,4 @@ class TestContactSheet(object):
         max_images = 100
 
         # act
-        contact_sheet = contact_sheet.make_contact_sheet(img_folder, output_file, max_images= max_images, column_count = 10)
+        contact_sheet = make_contact_sheet(img_folder, output_file, max_images= max_images, column_count = 10)
