4ff82b19acadb9e4478d0080cff4518fc4ec628b
aadennis <dennis.circ@gmail.com>
2023-07-05 14:12:10 +0100

    Tidy the code
    TODO - still a bit too eager. eg. a picture
    of an allotment is claimed to have the words
    peat and dink. Peat surely a coincidence!

--- ImageHandling/image_processing/src/text_checker_v3.py
+++ ImageHandling/image_processing/src/text_checker_v3.py
@@ -10,16 +10,8 @@ english_words = set(nltk.corpus.words.words())
 
 pytesseract.pytesseract.tesseract_cmd = r'C:/Program Files/Tesseract-OCR/tesseract.exe'
 
-for i in range(3, 5):  # Assuming you have 10 images from a01.jpg to a10.jpg
+for i in range(1, 6):  # Assuming you have 10 images from a01.jpg to a10.jpg
     image_path = f'tests/a{i:02d}.jpg'  # Assuming the images are in the same directory as your Python script
-    image2 = f'tests/out_{i:02d}.jpg'
-
-
-    #Read the image using OpenCV
-    #image = cv2.imread(image_path) # orig
-    # Convert the image to grayscale
-    #gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY) # orig
-    # Apply OCR using Pytesseract
     image = cv2.imread(image_path)
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     blur = cv2.GaussianBlur(gray, (3,3), 0)
@@ -36,33 +28,9 @@ for i in range(3, 5):  # Assuming you have 10 images from a01.jpg to a10.jpg
     words = extracted_text.split()
     filtered_words = [word for word in words if len(word) >= 4 and word.lower() in english_words]
 
-    print(filtered_words)
-
-    #cv2.imshow('thresh', thresh)
-    #cv2.imshow('opening', opening)
-    #cv2.imshow('invert', invert)
-    #cv2.waitKey()
-     # Check if text was detected or not
     if filtered_words:
         print(f'{image_path} contains text.')
+        print(filtered_words)
+        print("-------------------------")
     else:
         print(f'{image_path} does not contain text.')
-    #text = pytesseract.image_to_string(Image.open(image_path), lang='eng', 
-     #                                  config='--psm 10 --oem 3 -c tessedit_char_whitelist=0123456789') # orig
-
-    # im = Image.open(image_path)
-    # im = im.filter(ImageFilter.MedianFilter())
-    # enhancer = ImageEnhance.Contrast(im)
-    # im = enhancer.enhance(2)
-    # im = im.convert('1')
-    # im.save(image2)
-    # text = pytesseract.image_to_string(Image.open(image2))
-    # print(text)
-
-
-
-
-    #text = pytesseract.image_to_osd(gray)
-    
-
-   
