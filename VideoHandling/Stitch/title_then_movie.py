78e458029894a8fc646adc75b430ef9d2f65f383
aadennis <dennis.circ@gmail.com>
2023-08-28 11:23:11 +0100

    use video files for image clips

--- VideoHandling/Stitch/title_then_movie.py
+++ VideoHandling/Stitch/title_then_movie.py
@@ -1,34 +1,29 @@
-from moviepy.editor import VideoFileClip, ImageClip, concatenate_videoclips
+"""
+POC that combines a still image and a video clip, and writes the
+output to a video file.
+So, if 2 video clips are cat'ed, fine.
+However... if a video clip and an image clip are cat'ed, then whatever
+comes second, scrolls like mad.
+Docs at https://zulko.github.io/moviepy/ref/ref.html say that ImageClip
+is "just" a still version of a VideoClip... but tests suggest not quite.
+So in future, if I need an image and a duration as part of a larger video,
+I'll use Clipchamp or similar to convert images to videos with a given
+duration, and then do the concatenation.
+"""
+import random
+from moviepy.editor import VideoFileClip, concatenate_videoclips
 
-JPG_PATH = 'c:/tempx/piano.jpg' # save as test image
-MP4_PATH = 'c:/tempx/output.mp4'
-MP4_PATH2 = 'c:/tempx/shorty.mp4'
-
-OUTPUT_PATH = 'c:/tempx/output_videox3.mp4'
-CLIP_OUTPUT = 'c:/tempx/clip_output.mp4'
-IMAGE_DURATION=3
+CYCLE_PATH = 'c:/tempx/cycle_path.mp4'
+PIANO_STILL = 'c:/tempx/piano.mp4'
 CODEC = 'libx264'
 
-jpg_image = ImageClip(JPG_PATH, duration=IMAGE_DURATION)
-jpg_image.write_videofile(CLIP_OUTPUT, codec=CODEC, fps=30)
+OUTPUT_PATH = f'c:/tempx/cycle_with_piano_{random.randint(1,999)}.mp4'
 
-# Load the MP4 video
-image_clip = VideoFileClip(CLIP_OUTPUT)
-video_clip = VideoFileClip(MP4_PATH)
-video_clip2 = VideoFileClip(MP4_PATH2)
+video_clip = VideoFileClip(CYCLE_PATH)
+video_clip2 = VideoFileClip(PIANO_STILL)
 
-# The next lines amount to this:
-# If 2 video clips are cat'ed, fine.
-# If a video clip and an image clip are cat'ed, then whatever
-# comes second, scrolls like mad. argh. Check it in anyway, as it's
-# learning of a sort.
-# Docs at https://zulko.github.io/moviepy/ref/ref.html say that ImageClip
-# is "just" a still version of a VideoClip... but tests suggest not quite.
-final = concatenate_videoclips([video_clip2, video_clip])
-#final = concatenate_videoclips([image_clip, video_clip])
-#final = concatenate_videoclips([video_clip2, jpg_image])
-
-final.write_videofile(OUTPUT_PATH,codec='libx264',fps=30)
+# Cat the 2 videos together, and write that out to file
+final = concatenate_videoclips([video_clip, video_clip2])
+final.write_videofile(OUTPUT_PATH,codec=CODEC,fps=30)
 
 print(f"Image-as-video is saved at [{OUTPUT_PATH}]")
-
