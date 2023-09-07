843373e309465c54ad6f2489b35918ccc7ba386e
aadennis <dennis.circ@gmail.com>
2023-09-07 23:21:57 +0100

    refactor

--- VideoHandling/Stitch/convert_video_format_to_another.py
+++ VideoHandling/Stitch/convert_video_format_to_another.py
@@ -8,7 +8,10 @@ from moviepy.editor import VideoFileClip
 SRC_ROOT = "d:/Sandbox/git/aadennis/PythonSandboxAA/VideoHandling/Stitch/test/"
 ASSETS = SRC_ROOT + "assets/"
 TARGET = SRC_ROOT + "output/"
-SOURCE_WILD_CARD = "bof*.mp4"
+SOURCE_WILD_CARD = "bof*"
+SOURCE_FILE_TYPE = ".mp4"
+TARGET_FILE_TYPE = ".avi"
+CODEC = 'libx264'
 
 
 def get_filename_no_ext(path):
@@ -19,9 +22,8 @@ def get_filename_no_ext(path):
     """
     return os.path.splitext(os.path.basename(path))[0]
 
-# Convert mp4 files to avi format
-for file in glob.iglob(ASSETS + SOURCE_WILD_CARD):
+for file in glob.iglob(ASSETS + SOURCE_WILD_CARD + SOURCE_FILE_TYPE):
     filename = get_filename_no_ext(file)
-    avi_file = TARGET + filename + ".avi"
+    avi_file = TARGET + filename + TARGET_FILE_TYPE
     clip = VideoFileClip(file)
-    clip.write_videofile(avi_file, codec='libx264')
+    clip.write_videofile(avi_file, codec=CODEC)
