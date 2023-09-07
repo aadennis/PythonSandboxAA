c11ea351ef60a790135ef8e827d0d921f40b7d4b
aadennis <dennis.circ@gmail.com>
2023-09-07 23:17:35 +0100

    refactor

--- VideoHandling/Stitch/convert_video_format_to_another.py
+++ VideoHandling/Stitch/convert_video_format_to_another.py
@@ -10,10 +10,18 @@ ASSETS = SRC_ROOT + "assets/"
 TARGET = SRC_ROOT + "output/"
 SOURCE_WILD_CARD = "bof*.mp4"
 
+
+def get_filename_no_ext(path):
+    """
+        Given a full path name, return just the filename, 
+        without its extension.
+        For example, "c:/temp/test.txt" returns "test"
+    """
+    return os.path.splitext(os.path.basename(path))[0]
+
 # Convert mp4 files to avi format
-for mp4_file in glob.iglob(ASSETS + SOURCE_WILD_CARD):
-    a = os.path.basename(mp4_file)
-    b = os.path.splitext(a)[0]
-    avi_file = TARGET + b + ".avi"
-    clip = VideoFileClip(mp4_file)
+for file in glob.iglob(ASSETS + SOURCE_WILD_CARD):
+    filename = get_filename_no_ext(file)
+    avi_file = TARGET + filename + ".avi"
+    clip = VideoFileClip(file)
     clip.write_videofile(avi_file, codec='libx264')
