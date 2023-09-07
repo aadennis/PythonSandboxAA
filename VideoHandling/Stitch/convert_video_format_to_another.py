bac1def0e2ff2ac6ee8d106256822c8123bf306a
aadennis <dennis.circ@gmail.com>
2023-09-07 23:09:05 +0100

    separate source and target test folders

--- VideoHandling/Stitch/convert_video_format_to_another.py
+++ VideoHandling/Stitch/convert_video_format_to_another.py
@@ -1,14 +1,23 @@
 """
     Convert all video files of one format to another format
 """
+import os
 import glob
 from moviepy.editor import VideoFileClip
 
-SRC_ROOT = "d:/Sandbox/git/aadennis/PythonSandboxAA/VideoHandling/Stitch/test/assets/"
+SRC_ROOT = "d:/Sandbox/git/aadennis/PythonSandboxAA/VideoHandling/Stitch/test/"
+ASSETS = SRC_ROOT + "assets/"
+TARGET = SRC_ROOT + "output/"
+SOURCE_WILD_CARD = "bof*.mp4"
 
 # Convert mp4 files to avi format
-for mp4_file in glob.iglob(SRC_ROOT + "bof*.mp4"):
+for mp4_file in glob.iglob(ASSETS + SOURCE_WILD_CARD):
     print(mp4_file)
-    avi_file = mp4_file.replace('.mp4', '.avi')
+    a = os.path.basename(mp4_file)
+    print(a)
+    b = os.path.splitext(a)[0]
+    print(b)
+    avi_file = TARGET + b + ".avi"
+    print(avi_file)
     clip = VideoFileClip(mp4_file)
     clip.write_videofile(avi_file, codec='libx264')
