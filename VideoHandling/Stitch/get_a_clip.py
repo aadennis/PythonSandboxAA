a51f08f6934fb13bb3e6c085b6a88a7337ee79bf
aadennis <dennis.circ@gmail.com>
2023-10-10 22:23:36 +0100

    cos

--- VideoHandling/Stitch/get_a_clip.py
+++ VideoHandling/Stitch/get_a_clip.py
@@ -8,13 +8,13 @@ https://moviepy.readthedocs.io/en/latest/getting_started/quick_presentation.html
 from moviepy.editor import VideoFileClip
 from moviepy import *
 
-START_CLIP = "00:00:50"
-END_CLIP = "00:01:00"
+START_CLIP = "00:02:55"
+END_CLIP = "00:03:30"
 
-INPUT_VIDEO = "c:/VideoStaging/mp4Joined.mp4"
+INPUT_VIDEO = r"C:\VideoStaging\Sea 25-09-2023\Sea_25-09-2023.mp4"
 # also webm, etc. But note that mp4 for 10 seconds in this consumes 10mb, that is
 # 1mb per second. webm uses only 2mb, so 20% of the mp4.
-OUTPUT_VIDEO = "c:/VideoStaging/shorty.mp4" 
+OUTPUT_VIDEO = r"C:\VideoStaging\Sea 25-09-2023\Sea_25-09-2023_short1.mp4" 
 
 CLIP = VideoFileClip(INPUT_VIDEO).subclip(START_CLIP, END_CLIP)
 CLIP.write_videofile(OUTPUT_VIDEO)
