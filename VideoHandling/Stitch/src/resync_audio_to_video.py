1ff7d3797b5b5cb84d5ff0436b878bc0a64354cb
aadennis <dennis.circ@gmail.com>
2023-10-11 11:31:15 +0100

    Give test output file unique-ish name

--- VideoHandling/Stitch/src/resync_audio_to_video.py
+++ VideoHandling/Stitch/src/resync_audio_to_video.py
@@ -2,12 +2,12 @@
     Fix audio vs video sync issues.
 """
 from moviepy.editor import VideoFileClip
+from faker import Faker
 
 # SRC_ROOT = "VideoHandling/Stitch/test/" # if opened from PythonSandboxAA
 SRC_ROOT = "test/"  # if opened from Stitch
 ASSETS = SRC_ROOT + "assets/"
 TARGET = SRC_ROOT + "output/"
-TEST_FILE = "townride_no_text_10secs.mp4"
 
 def resync(input_video, time_offset):
     """
@@ -21,23 +21,21 @@ def resync(input_video, time_offset):
     video = video_clip.subclip(0, video_clip.duration - time_offset)
     audio = video_clip.audio.subclip(max(0, time_offset), video_clip.duration)
     video = video.set_audio(audio)
-
     return video
 
 def resync_audio_to_video(input_file, output_file, time_offset = 1):
     """
-        See [get_frames_from_video] for full description.
-        Call get_frames_from_video, passing through the same parameters.
+        See [resync] for full description.
+        Call resync, passing through the same parameters.
         Write the returned VideoClip to a file
     """
     frame_set = resync(input_file, time_offset)
     frame_set.write_videofile(output_file, codec='libx264', fps=24)
 
-
 if __name__ == "__main__":
 
-    test_input_file = ASSETS + '/golf_out_of_sync_audio.mp4'
-    #test_input_file = ASSETS + '/lip_sync_ok.MOV'
-    test_output_file = "c:/temp/malips.mp4"
-
-    resync_audio_to_video(test_input_file, test_output_file)
+    TEST_INPUT_FILE = ASSETS + '/golf_out_of_sync_audio.mp4'
+    faker = Faker()
+    test_output_file = TARGET + Faker().last_name() +  '.mp4'
+    resync_audio_to_video(TEST_INPUT_FILE, test_output_file, 0.9)
+    print(f"Test output is in [{test_output_file}]")
