f1323644352d1f09e1247425386075e8f039258d
aadennis <dennis.circ@gmail.com>
2023-11-19 13:02:01 +0000

    Add visual cues to spot data errors

--- TideTimes/src/mid_tides.py
+++ TideTimes/src/mid_tides.py
@@ -11,20 +11,23 @@ def format_tide_dictation(input_text):
         fail to hear Low and High. Ditto Bravo for "new line". For this, the code
         ignores it, and uses the token separator of space to replace with newline.
         An example of input, taken from (flawed) output from dictation, is below 
-        in test_text_1.
+        in test_text_1. test_text_2 is good dictation.
     """    
     translation_dict = {'lima': 'low', 'hotel': 'high'}
 
     words = input_text.lower().split()
     translated_words = []
 
+    current_date = 1
     for i, word in enumerate(words):
         if word == 'bravo': # ignore - just visual punctuation for the dictator
             continue
         if word in translation_dict:
             translated_words.append(translation_dict[word])
         else:
-            translated_words.append(word)
+            # must be data for a day
+            translated_words.append(f"{current_date}-{word}")
+            current_date += 1
 
     translated_text = ' '.join(translated_words).replace(' ','\n')
     return translated_text
@@ -34,5 +37,17 @@ test_text_1 = """
  bravo 2B101731163813B110930173014B 121229183114 bravo hotel 070115132329 
  bravo 081214143430 bravo 091512153532 bravo 101110163134 bravo 110108172235 
  bravo hotel 110236170809 bravo"""
-output_text = format_tide_dictation(test_text_1)
+
+test_text_2 = """
+Hotel 082838145008 bravo 085936152210 bravo 093534155612 bravo
+ 101731163813 bravo 110930173014 bravo Lima 055015121229 bravo
+   070115132329 bravo 081214143430 bravo 091512153532 bravo 
+   101010163134 bravo 110108172235 bravo Hotel 054837114907 bravo 
+   063439123806 bravo 071640132405 bravo 075640140805 bravo 083840144906 
+   bravo 092339153206 bravo 101137161808 bravo 110236170809 bravo Lima 
+   052711120034 bravo 063011130433 bravo 074911141433 bravo 090410152233 bravo 
+   101009162534 bravo 110808172235 bravo hotel 054637120007 bravo 063238124706 
+   bravo 071239133106 bravo 074638140807 bravo 081838144007 
+   bravo 085137151008 bravo"""
+output_text = format_tide_dictation(test_text_2)
 print(output_text)
