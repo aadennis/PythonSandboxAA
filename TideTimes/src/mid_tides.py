9f3ba32e6c0d24207e7254d6ec44dbc8754494d6
aadennis <dennis.circ@gmail.com>
2023-11-21 20:02:34 +0000

    remove old test data

--- TideTimes/src/mid_tides.py
+++ TideTimes/src/mid_tides.py
@@ -70,7 +70,6 @@ def format_tide_dictation(input_text, month_year):
     words = input_text.lower().split()
     tide_days = []
 
-
     current_date = 0
     for i, tide_day in enumerate(words):
         if tide_day == 'bravo': # ignore - just visual punctuation for the dictator
@@ -86,29 +85,9 @@ def format_tide_dictation(input_text, month_year):
     for tide_day in tide_days:
         formatted_tide_month += f"{tide_day.print()}\n"
         
-
-    #translated_text = ' '.join(translated_words).replace(' ','\n')
     return formatted_tide_month
 
 test_text_1 = """
- Lima 082838145008 bravo 085936152210 bravo 093534155672
- bravo 2B101731163813B110930173014B 121229183114 bravo hotel 070115132329 
- bravo 081214143430 bravo 091512153532 bravo 101110163134 bravo 110108172235 
- bravo hotel 110236170809 bravo"""
-
-test_text_2 = """
-Hotel 082838145008 bravo 085936152210 bravo 093534155612 bravo
- 101731163813 bravo 110930173014 bravo Lima 055015121229 bravo
-   070115132329 bravo 081214143430 bravo 091512153532 bravo 
-   101010163134 bravo 110108172235 bravo Hotel 054837114907 bravo 
-   063439123806 bravo 071640132405 bravo 075640140805 bravo 083840144906 
-   bravo 092339153206 bravo 101137161808 bravo 110236170809 bravo Lima 
-   052711120034 bravo 063011130433 bravo 074911141433 bravo 090410152233 bravo 
-   101009162534 bravo 110808172235 bravo hotel 054637120007 bravo 063238124706 
-   bravo 071239133106 bravo 074638140807 bravo 081838144007 
-   bravo 085137151008 bravo"""
-
-test_text_3 = """
 082838145008 bravo 085936152210 bravo 093534155612 bravo
  101731163813 bravo 110930173014 bravo 055015121229 bravo
    070115132329 bravo 081214143430 bravo 091512153532 bravo 
@@ -120,5 +99,5 @@ test_text_3 = """
    bravo 071239133106 bravo 074638140807 bravo 081838144007 
    bravo 085137151008 bravo"""
 
-output_text = format_tide_dictation(test_text_3, "12/2023")
+output_text = format_tide_dictation(test_text_1, "12/2023")
 print(output_text)
