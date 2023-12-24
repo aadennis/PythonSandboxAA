e9d4c028bff3cb27b11d97ae70b67de0e0db83aa
aadennis <dennis.circ@gmail.com>
2023-12-24 07:46:13 +0000

    pronunciation tests ok

--- PomodoroStuff/pomodorov2.py
+++ PomodoroStuff/pomodorov2.py
@@ -3,17 +3,36 @@ import pyttsx3
 import datetime
 import time
 
-def announce_time():
+def announce_time(now_time = datetime.datetime.now()):
     engine = pyttsx3.init()
 
     while True:
-        now = datetime.datetime.now()
+        now = now_time
         if now.minute % 15 == 0:
-            hour_now = now.strftime("%H")
-            engine.say(f"It is {hour_now} o'clock")
+            hour_now = int(now.strftime("%H"))
+            am_pm = now.strftime("%p")
+            minutes = ""
+            if am_pm == 'PM':
+                hour_now = hour_now - 12
+            else: # engine cannot pronounce 'AM'
+                am_pm = "eh em"
+            if now.minute > 0:
+                minutes = str(now.minute)
+            engine.say(f"It is {hour_now} {minutes} {am_pm}")
             engine.runAndWait()
         time.sleep(60)
 
 if __name__ == "__main__":
-    announce_time()
+    year=2023
+    month=11
+    day=30
+    hour=14
+    minute=0
+    #minute=15
+    #minute=30
+    #minute=45
+    
+
+    now = datetime.datetime(year=year, month=month, day=day, hour=hour, minute=minute)
+    announce_time(now)
 
