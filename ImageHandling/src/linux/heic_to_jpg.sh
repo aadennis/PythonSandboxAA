4a1fbe83bba5cee9f4f968312c7158c7f232c0a8
dennis <dennis.circ@gmail.com>
2024-05-11 20:13:01 +0100

    heic work

---
+++ ImageHandling/src/linux/heic_to_jpg.sh
@@ -0,0 +1,15 @@
+# WARNING: Make a backup copy of all your images before running this code.
+extension=HEIC
+pwd
+image_file_dir=../../tests/images
+cd $image_file_dir
+echo "adsfafff"
+pwd
+for inFile in `ls *.${extension}`
+do
+    baseFilename=`basename $inFile .${extension}`
+    outFile="${baseFilename}.jpg"
+    echo "$inFile --> $outFile"
+    magick $inFile $outFile
+    mogrify -resize 50% $outFile   # Resize the image to 50% of the original size
+done
