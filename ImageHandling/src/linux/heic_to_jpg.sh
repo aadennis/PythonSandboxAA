ea6599cc800c318f66a2427498fdca324dd3c677
aadennis <dennis.circ@gmail.com>
2024-05-12 16:32:36 +0100

    docs

--- ImageHandling/src/linux/heic_to_jpg.sh
+++ ImageHandling/src/linux/heic_to_jpg.sh
@@ -2,6 +2,8 @@
 
 # WARNING: Make a backup copy of all your images before running this code.
 # Make a copy of all .heic files in the given folder, in .jpg format
+# usage: bash ./heic_to_jpg.sh
+
 extension=HEIC
 pwd
 image_file_dir=../../tests/TestImageFiles
