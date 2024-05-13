9ae11c6fdbcfeec31e3d676191f043242ed4e89c
aadennis <dennis.circ@gmail.com>
2024-05-13 11:38:13 +0100

    Hardcode HEIC - script is dedicated to this

--- ImageHandling/src/linux/heic_to_jpg.sh
+++ ImageHandling/src/linux/heic_to_jpg.sh
@@ -1,22 +1,21 @@
 #!/bin/zsh
 
 # WARNING: Make a backup copy of all your images before running this code.
-# Make a copy of all .heic files in the given folder, in .jpg format
-# usage: bash ./heic_to_jpg.sh
+# Make a copy of all .heic files in the given folder, in .jpg format.
+# Example: if a source file is [football.heic], 
+#          the target file is [football.heic.jpg]
 
-# Source the config file
+# usage: bash ./heic_to_jpg.sh
+#------------------------------------------------------------
 source config.cfg
 
-extension=HEIC
-pwd
-echo $image_file_dir
 cd $image_file_dir
 echo "Current folder..."
 pwd
 shopt -s nocaseglob  # Enable case-insensitive filename expansion
-for inFile in `ls *.${extension}`
+for inFile in `ls *.HEIC`
 do
-    baseFilename=`basename $inFile .${extension}`
+    baseFilename=`basename $inFile .HEIC`
     outFile="${baseFilename}.jpg"
     echo "Converting [$inFile] --> [$outFile]"
     magick $inFile $outFile
