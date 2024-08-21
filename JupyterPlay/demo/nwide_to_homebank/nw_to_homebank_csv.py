80d6bad5d03e0e4aac2d9a0e2d2eba0ef97f6608
aadennis <dennis.circ@gmail.com>
2024-08-21 22:08:06 +0100

    testable start

--- JupyterPlay/demo/nwide_to_homebank/nw_to_homebank_csv.py
+++ JupyterPlay/demo/nwide_to_homebank/nw_to_homebank_csv.py
@@ -91,7 +91,62 @@ def convert_nw_transactions():
         print(out_file)
         convert_nw_to_homebank_csv(f, out_file)
 
-convert_nw_transactions()
+# - refactor for testing wip...
+
+def read_nw_csv(file_path, encoding='cp1252'):
+    """Reads the Nationwide CSV file and returns a DataFrame."""
+    return pd.read_csv(file_path, skiprows=4, encoding=encoding)
+
+def preprocess_data(input_df):
+    """Performs basic data manipulation on the input DataFrame."""
+    output_df = pd.DataFrame({
+        "Date": pd.to_datetime(input_df["Date"], format="%d %b %Y"),
+        "payment type": 1,  # placeholder
+        "info": "info 1",  # placeholder
+        "Transactions": input_df["Transactions"],
+        "Location": input_df["Location"],
+        "Paid out": input_df["Paid out"].astype(str).str.replace("£", ""),
+        "Paid in": input_df["Paid in"].astype(str).str.replace("£", ""),
+        "category": "category 1",  # placeholder
+        "Tags": "tag1 tag2"  # placeholder
+    })
+    return output_df
+
+def add_transaction_info(output_df):
+    """Adds transaction info to the output DataFrame."""
+    output_df.loc[0, 'info'] = 'First transaction in statement'
+    output_df.loc[output_df.index[-1], 'info'] = 'Last transaction in statement'
+    return output_df
+
+def reorder_columns(output_df):
+    """Reorders columns in the output DataFrame."""
+    desired_order = [0, 1, 2, 3, 4, 7, 5, 6]
+    return output_df.iloc[:, desired_order]
+
+def convert_nw_to_homebank_csv_v2(in_file, out_file):
+    input_df = read_nw_csv(in_file)
+    output_df = preprocess_data(input_df)
+    output_df = add_transaction_info(output_df)
+    output_df = reorder_columns(output_df)
+    output_df.to_csv(out_file, sep=";", index=False, header=False)
+
+def convert_nw_transactions_v2():
+    config_data = read_config()
+    in_dir = config_data['cc_txn_source_path']
+    print(in_dir)
+    for f in glob.iglob(f'{in_dir}/*Statement Download*.csv'):
+        print(f)
+        file_name = os.path.basename(f)
+        out_file=f'{in_dir}/{file_name[0:2]}_outputx.csv'
+        print(out_file)
+        convert_nw_to_homebank_csv_v2(f, out_file)
+
+# Example usage:
+config = read_config()
+#convert_nw_to_homebank_csv("input_nw.csv", "output_homebank.csv")
+
+
+
+convert_nw_transactions_v2()
 
 #create_config()
-
