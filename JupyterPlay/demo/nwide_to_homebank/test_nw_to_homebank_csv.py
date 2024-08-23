df29cdcc9ca0b1cba30368db076b69ffc6aebd82
aadennis <dennis.circ@gmail.com>
2024-08-23 20:57:07 +0100

    formatting

--- JupyterPlay/demo/nwide_to_homebank/test_nw_to_homebank_csv.py
+++ JupyterPlay/demo/nwide_to_homebank/test_nw_to_homebank_csv.py
@@ -9,33 +9,32 @@ class TestPreprocessData(unittest.TestCase):
             "Date": ["01 Jan 2022", "31 Jan 2022", "02 Feb 2022"],
             "Transactions": ["Purchase A", "Purchase B", "Purchase C"],
             "Location": ["London", "Manchester", "London"],
-            "Paid out": ["£100.00", "","£254.23"],
+            "Paid out": ["£100.00", "", "£254.23"],
             "Paid in": ["", "£10.00", ""]
         }
         nw_imported_data = pd.DataFrame({
-            "Date": pd.to_datetime(["2022-01-01", "2022-01-31","2022-02-02"]),
+            "Date": pd.to_datetime(["2022-01-01", "2022-01-31", "2022-02-02"]),
             "payment type": [1, 1, 1],
-            "info": ["info 1", "info 1","info 1"],
+            "info": ["info 1", "info 1", "info 1"],
             "Transactions": ["Purchase A", "Purchase B", "Purchase C"],
             "Location": ["London", "Manchester", "London"],
             "Paid out": ["100.00", "", "254.23"],
-            "Paid in": ["", "10.00",""],
+            "Paid in": ["", "10.00", ""],
             "category": ["category 1", "category 1", "category 1"],
             "Tags": ["tag1 tag2", "tag1 tag2", "tag1 tag2"],
         })
         self.input_df = pd.DataFrame(nw_src_data)
         self.nw_imported_data = pd.DataFrame(nw_imported_data)
 
-
     def test_preprocess_data(self):
         expected_output = pd.DataFrame({
-            "Date": pd.to_datetime(["2022-01-01", "2022-01-31","2022-02-02"]),
+            "Date": pd.to_datetime(["2022-01-01", "2022-01-31", "2022-02-02"]),
             "payment type": [1, 1, 1],
-            "info": ["info 1", "info 1","info 1"],
+            "info": ["info 1", "info 1", "info 1"],
             "Transactions": ["Purchase A", "Purchase B", "Purchase C"],
             "Location": ["London", "Manchester", "London"],
             "Paid out": ["100.00", "", "254.23"],
-            "Paid in": ["", "10.00",""],
+            "Paid in": ["", "10.00", ""],
             "category": ["category 1", "category 1", "category 1"],
             "Tags": ["tag1 tag2", "tag1 tag2", "tag1 tag2"],
         })
@@ -47,13 +46,13 @@ class TestPreprocessData(unittest.TestCase):
 
     def test_first_last_in_statement(self):
         expected_output = pd.DataFrame({
-            "Date": pd.to_datetime(["2022-01-01", "2022-01-31","2022-02-02"]),
+            "Date": pd.to_datetime(["2022-01-01", "2022-01-31", "2022-02-02"]),
             "payment type": [1, 1, 1],
-            "info": ["First transaction in statement", "info 1","Last transaction in statement"],
+            "info": ["First transaction in statement", "info 1", "Last transaction in statement"],
             "Transactions": ["Purchase A", "Purchase B", "Purchase C"],
             "Location": ["London", "Manchester", "London"],
             "Paid out": ["100.00", "", "254.23"],
-            "Paid in": ["", "10.00",""],
+            "Paid in": ["", "10.00", ""],
             "category": ["category 1", "category 1", "category 1"],
             "Tags": ["tag1 tag2", "tag1 tag2", "tag1 tag2"],
         })
@@ -61,6 +60,7 @@ class TestPreprocessData(unittest.TestCase):
         actual_output = add_transaction_info(self.nw_imported_data)
 
         pd.testing.assert_frame_equal(expected_output, actual_output)
+
 
 if __name__ == "__main__":
     unittest.main()
