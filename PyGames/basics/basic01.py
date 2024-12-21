791192c901e2333916a67389be12c0ee4fec724f
aadennis <denniswells@gmail.com>
2024-12-21 21:45:48 +0000

    basics

---
+++ PyGames/basics/basic01.py
@@ -0,0 +1,23 @@
+#credit pygame.org
+
+import pygame
+
+pygame.init()
+screen = pygame.display.set_mode((1280, 720))
+clock = pygame.time.Clock()
+running = True
+
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    screen.fill("purple")
+
+    # RENDER YOUR GAME HERE
+
+    pygame.display.flip()
+
+    clock.tick(60) 
+
+pygame.quit()
