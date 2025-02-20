3535456a3d6f81041896e33cd336a38fa9686da1
aadennis <dennis.circ@gmail.com>
2025-02-20 20:56:25 +0000

    Now ok for titles

--- WebScraping/GetKindleCatalog.py
+++ WebScraping/GetKindleCatalog.py
@@ -1,4 +1,5 @@
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 import undetected_chromedriver as uc
 
 driver = uc.Chrome()
@@ -10,7 +11,7 @@ driver.get("https://www.amazon.co.uk/hz/mycd/digital-console/contentlist/booksAl
 input("Log in manually, then press Enter here...")
 
 # Find all the book titles using the class 'digital_entity_title'
-titles = driver.find_elements_by_css_selector(".digital_entity_title")
+titles = driver.find_elements(By.CSS_SELECTOR, ".digital_entity_title")
 
 # Check if any titles are found
 if not titles:
