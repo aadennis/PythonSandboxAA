6043ba9af1a176fa83d56018d803cdb4ec694651
aadennis <dennis.circ@gmail.com>
2025-04-05 22:23:14 +0100

    I want a single note in each track, not 12

--- MusicHandling/create_midi_file_template.py
+++ MusicHandling/create_midi_file_template.py
@@ -4,22 +4,19 @@ from mido import Message, MidiFile, MidiTrack
 mid = MidiFile(ticks_per_beat=480)  # Standard PPQ setting
 tempo = 60000000 // 95  # 95 BPM
 
-# Function to create a MIDI track with C4 half-notes
-def create_track():
+# Function to create a MIDI track with a unique channel
+def create_track(channel):
     track = MidiTrack()
-    for bar in range(12):
-        # Note on (C4 = MIDI note 60)
-        track.append(Message('note_on', note=60, velocity=64, time=0))
-        # Note off (after half-note duration)
-        track.append(Message('note_off', note=60, velocity=64, time=480))  # Half note at 95 BPM
+    track.append(Message('program_change', program=0, channel=channel))  # Assign an instrument (Acoustic Grand Piano)
+    for bar in range(1):
+        track.append(Message('note_on', note=60, velocity=64, time=0, channel=channel))
+        track.append(Message('note_off', note=60, velocity=64, time=480, channel=channel))  # Half note
     return track
 
-# Create 3 tracks
-for _ in range(3):
-    mid.tracks.append(create_track())
+# Create 3 tracks with different channels
+for i in range(3):
+    mid.tracks.append(create_track(i))  # Channels 0, 1, 2
 
 # Save the MIDI file
-mid.save("template.mid")
-print("MIDI file saved as template.mid")
-
-
+mid.save("template_fixed.mid")
+print("MIDI file saved as template_fixed.mid")
