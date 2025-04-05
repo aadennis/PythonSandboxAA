d5f22cfd18aa38bb5ceb53c70aafb0ac0c31f745
aadennis <dennis.circ@gmail.com>
2025-04-05 22:40:36 +0100

    3/4 time, single note

--- MusicHandling/create_midi_file_template.py
+++ MusicHandling/create_midi_file_template.py
@@ -1,16 +1,25 @@
-from mido import Message, MidiFile, MidiTrack
+from mido import Message, MidiFile, MidiTrack, MetaMessage
 
 # Create a new MIDI file
 mid = MidiFile(ticks_per_beat=480)  # Standard PPQ setting
 tempo = 60000000 // 95  # 95 BPM
 
-# Function to create a MIDI track with a unique channel
+# Function to create a MIDI track with 3/4 time signature and unique channel
 def create_track(channel):
     track = MidiTrack()
-    track.append(Message('program_change', program=0, channel=channel))  # Assign an instrument (Acoustic Grand Piano)
+    
+    # Set instrument (Acoustic Grand Piano)
+    track.append(Message('program_change', program=0, channel=channel))
+    
+    # Set time signature to 3/4
+    track.append(MetaMessage('time_signature', numerator=3, denominator=4, time=0))
+    
     for bar in range(1):
+        # Note on (C4 = MIDI note 60)
         track.append(Message('note_on', note=60, velocity=64, time=0, channel=channel))
-        track.append(Message('note_off', note=60, velocity=64, time=480, channel=channel))  # Half note
+        # Note off (half note duration in 3/4 time)
+        track.append(Message('note_off', note=60, velocity=64, time=480, channel=channel))  
+    
     return track
 
 # Create 3 tracks with different channels
@@ -18,5 +27,5 @@ for i in range(3):
     mid.tracks.append(create_track(i))  # Channels 0, 1, 2
 
 # Save the MIDI file
-mid.save("template_fixed.mid")
-print("MIDI file saved as template_fixed.mid")
+mid.save("template_3-4.mid")
+print("MIDI file saved as template_3-4.mid")
