5012831da138c6b5e6ee21b526f6bd035d457526
aadennis <dennis.circ@gmail.com>
2025-08-12 20:35:25 +0100

    refactor

--- JsonPlay/json_play.py
+++ JsonPlay/json_play.py
@@ -12,13 +12,17 @@ def coerce_to_int64(df: pd.DataFrame, *columns: str) -> pd.DataFrame:
         df[col] = pd.to_numeric(df[col], errors='coerce').astype('Int64')
     return df
 
+def csv_to_json():
 
-df = pd.read_csv('rulers2.csv') 
-# Optional: inspect the DataFrame
-print(df.head())
+    df = pd.read_csv('rulers2.csv') 
+    # Optional: inspect the DataFrame
+    print(df.head())
 
-df = coerce_to_int64(df, 'Reign_Start', 'Reign_End')
-print(df.head(10))
+    df = coerce_to_int64(df, 'Reign_Start', 'Reign_End')
+    print(df.head(10))
 
-# Write the DataFrame to a JSON file
-df.to_json('rulers2.json', orient='records', indent=4)
+    # Write the DataFrame to a JSON file
+    df.to_json('rulers2.json', orient='records', indent=4)
+
+if __name__ == '__main__':
+    csv_to_json()
