5a16f3ffae368fdb344c9534895d7671bee72c69
aadennis <dennis.circ@gmail.com>
2025-08-12 18:11:33 +0100

    fix reign_end displaying as decimal

--- JsonPlay/json_play.py
+++ JsonPlay/json_play.py
@@ -1,9 +1,15 @@
 import pandas as pd
 
-df = pd.read_csv('rulers2.csv')
-
+df = pd.read_csv('rulers2.csv') 
 # Optional: inspect the DataFrame
 print(df.head())
+
+# without this, default can display as a decimal, not an integer
+df['Reign_Start'] = pd.to_numeric(df['Reign_Start'],errors='coerce').astype('Int64')
+df['Reign_End'] = pd.to_numeric(df['Reign_End'],errors='coerce').astype('Int64')
+
+
+print(df.head(10))
 
 # Write the DataFrame to a JSON file
 df.to_json('rulers2.json', orient='records', indent=4)
