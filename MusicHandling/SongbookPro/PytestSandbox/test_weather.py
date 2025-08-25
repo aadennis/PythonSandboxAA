1019c4252737c8242d7e3f28ab60b3b97a49ce3b
aadennis <dennis.circ@gmail.com>
2025-08-25 20:07:05 +0100

    deliberate fail

--- MusicHandling/SongbookPro/PytestSandbox/test_weather.py
+++ MusicHandling/SongbookPro/PytestSandbox/test_weather.py
@@ -1,11 +1,14 @@
 from unittest.mock import patch
 from weather import get_weather
 
-def test_get_weather_mocked():
+def test_get_weather_calls_requests_correctly():
     mock_response = {"temp": "20°C", "condition": "Sunny"}
 
     with patch("weather.requests.get") as mock_get:
+        # arrange
         mock_get.return_value.json.return_value = mock_response
-
+        #act
         result = get_weather("London")
+        #assert
         assert result == mock_response
+        mock_get.assert_called_once_with("https://api.weather.com/Londonx")
